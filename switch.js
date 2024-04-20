f60c702aa39b76ef0644baba36df25ef1e37a3f0
Walker Milhoan <walker@milhoandesign.com>
2024-04-20 09:41:43 -0600

    Troubleshooting code

--- switch.js
+++ switch.js
@@ -1,10 +1,33 @@
-
-const select = document.querySelector('select');
+const select = document.querySelector('#theme'); // Added '#' to target by ID
 const html = document.querySelector('html');
 
-function update (bgColor, textColor) {
-    html.style.backgroundColor.bgColor; 
-    html.style.color.textColor;
-};
+select.addEventListener('change', function() {
+    update(select.value);
+});
 
-
+function update(theme) {
+    switch (theme) {
+        case 'white':
+            html.style.backgroundColor = 'white';
+            html.style.color = 'black';
+            break;
+        case 'black':
+            html.style.backgroundColor = 'black';
+            html.style.color = 'white';
+            break;
+        case 'purple':
+            html.style.backgroundColor = 'purple';
+            html.style.color = 'white';
+            break;
+        case 'yellow':
+            html.style.backgroundColor = 'yellow';
+            html.style.color = 'black';
+            break;
+        case 'psychedelic':
+            html.style.backgroundColor = 'pink';
+            html.style.color = 'lime';
+            break;
+        default:
+            break;
+    }
+}; 
