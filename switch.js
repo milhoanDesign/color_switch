0646a64568e9c3127b72f141c5bed466649a2edc
Walker Milhoan <walker@milhoandesign.com>
2024-04-20 09:19:52 -0600

    2nd commit

---
+++ switch.js
@@ -0,0 +1,10 @@
+
+const select = document.querySelector('select');
+const html = document.querySelector('html');
+
+function update (bgColor, textColor) {
+    html.style.backgroundColor.bgColor; 
+    html.style.color.textColor;
+};
+
+
